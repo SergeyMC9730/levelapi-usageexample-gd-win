56a70fbe84ba9f4094b9ffb8d60b5f1ec31383d9
SergeyMC9730 <bdsservers@yandex.ru>
2022-10-20 21:16:05 +0800

    Upload level, Upload everything from USER

--- src/main.cpp
+++ src/main.cpp
@@ -11,48 +11,169 @@
 
 // gd.h includes cocos2d.h
 #include <gd.h>
+#include <cocos-ext.h>
 
-void MenuLayer_onNewgrounds(gd::MenuLayer* self, cocos2d::CCObject* sender) {
-    std::cout << "cool!" << std::endl;
-    matdash::orig<&MenuLayer_onNewgrounds>(self, sender);
+// compile options
+#include "compile.h"
+
+#include <stdlib.h>
+
+using namespace cocos2d::extension;
+using namespace cocos2d;
+using namespace gd;
+
+cocos2d::CCObject* t;
+
+void sendLevelAPIMessage(const char* title, const char* description) {
+    AchievementNotifier * not = AchievementNotifier::sharedState();
+    not->notifyAchievement(title, description, "GJ_downloadBtn_001.png", true);
 }
 
-bool GJDropDownLayer_init(gd::GJDropDownLayer* self, const char* title, float height) {
-    return matdash::orig<&GJDropDownLayer_init>(self, "my own title", height * 0.5f);
+class NetworkAction {
+public:
+    static void post(std::string url, SEL_HttpResponse callback) {
+        if (DEBUG) std::cout << "[DEBUG] Send " << url << std::endl;
+        CCHttpClient* gdNetworking = CCHttpClient::getInstance();
+        CCHttpRequest* req = new CCHttpRequest();
+        req->setRequestType(CCHttpRequest::HttpRequestType::kHttpPost);
+        req->setUrl(url.c_str());
+        req->setResponseCallback(t, callback);
+        req->setRequestData(nullptr, 0);
+        gdNetworking->send(req);
+        req->release();
+        return;
+    }
+};
+
+class NTest {
+public:
+    void respCallback(CCHttpClient* client, CCHttpResponse* response) {
+        if (DEBUG) std::cout << "[LOG] " << response->isSucceed() << std::endl;
+        if (!response->isSucceed()) {
+            if (DEBUG) std::cout << "[LOG] " << response->getErrorBuffer() << std::endl;
+        }
+        else {
+            if (DEBUG) std::cout << "[LOG] " << response->getResponseData()->data() << std::endl;
+        }
+        CCScene *scn = CCDirector::sharedDirector()->getRunningScene();
+        gd::TextAlertPopup* p = gd::TextAlertPopup::create("Got something", 0.5f, 0.3f);
+        p->setPositionY(25.f);
+        scn->addChild(p, 1024);
+    }
+};
+
+namespace MenuLayerX {
+    bool MenuLayer_init(gd::MenuLayer* self) {
+        if (!matdash::orig<&MenuLayer_init>(self)) return false;
+        //NetworkAction::post("https://gd.dogotrigger.xyz/api/v1/search/lid/128", httpresponse_selector(NTest::respCallback));
+        return true;
+    }
+};
+namespace LevelInfoX {
+    int currentLevelID = 0;
+
+    class LIX {
+    public:
+        void respCallback(CCHttpClient* client, CCHttpResponse* response) {
+            if (response->isSucceed()) {
+                sendLevelAPIMessage("LevelAPI", "Level is queued!");
+            }
+            else {
+                sendLevelAPIMessage("LevelAPI", "LevelAPI is down");
+            }
+            return;
+        }
+        void onUpload(CCObject* target) {
+            sendLevelAPIMessage("LevelAPI", "Uploading level...");
+            std::string str = "https://gd.dogotrigger.xyz/api/v1/upload/level/";
+            if (DEBUG) std::cout << "[DEBUG] Upload level " << currentLevelID << std::endl;
+            str.append(std::to_string(currentLevelID));
+            NetworkAction::post(str, httpresponse_selector(LIX::respCallback));
+        }
+    };
+    bool init(gd::LevelInfoLayer *self, gd::GJGameLevel* selectedLevel) {
+        if (!matdash::orig<&init>(self, selectedLevel)) return false;
+
+        CCMenu* bmenu = CCMenu::create();
+        CCSprite* sprite = CCSprite::createWithSpriteFrameName("GJ_downloadBtn_001.png");
+        gd::CCMenuItemSpriteExtra* bbtn = gd::CCMenuItemSpriteExtra::create(sprite, self, menu_selector(LIX::onUpload));
+        bbtn->setPosition({ 150, 134 });
+        //bbtn->setScale(0.35f);
+        bmenu->addChild(bbtn);
+        
+        self->addChild(bmenu, 1024);
+
+        currentLevelID = selectedLevel->m_nLevelID;
+
+        return true;
+    }
 }
 
-matdash::cc::thiscall<void> PlayLayer_update(gd::PlayLayer* self, float dt) {
-    matdash::orig<&PlayLayer_update>(self, dt * 0.5f);
-    // this is due to the calling convention wrapper
-    // because matdash::cc::thiscall<void> is not actually void
-    // see https://github.com/matcool/mat-dash#usage for more info
-    return {};
+namespace ProfilePageX {
+    int accountID;
+
+    class PIX {
+    public:
+        void respCallback(CCHttpClient* client, CCHttpResponse* response) {
+            if (response->isSucceed()) {
+                sendLevelAPIMessage("LevelAPI", "Level is queued!");
+            }
+            else {
+                sendLevelAPIMessage("LevelAPI", "LevelAPI is down");
+            }
+            return;
+        }
+        void onUpload(CCObject* target) {
+            sendLevelAPIMessage("LevelAPI", "Moving levels into the queue...");
+            std::string str = "https://gd.dogotrigger.xyz/api/v1/upload/user/";
+            str.append(std::to_string(accountID));
+            NetworkAction::post(str, httpresponse_selector(PIX::respCallback));
+        }
+    };
+
+    bool init(gd::ProfilePage* self, int m_accountID, bool idk) {
+        if (!matdash::orig<&init>(self, accountID, idk)) return false;
+
+        accountID = m_accountID;
+
+        CCMenu* bmenu = CCMenu::create();
+        CCSprite* sprite = CCSprite::createWithSpriteFrameName("GJ_downloadBtn_001.png");
+        CCMenuItemSpriteExtra* bbtn = CCMenuItemSpriteExtra::create(sprite, self, menu_selector(PIX::onUpload));
+        bbtn->setPosition({ -194, -80 });
+        bmenu->addChild(bbtn);
+        self->addChild(bmenu, 1024);
+
+        return true;
+    }
 }
 
-bool MenuLayer_init(gd::MenuLayer* self) {
-    if (!matdash::orig<&MenuLayer_init>(self)) return false;
+namespace LevelBrowserX {
+    uint32_t* levelIDS = nullptr;
+    uint32_t levelIDSArraySize = 0;
+    bool buttonWasCreated = false;
+    bool UploadClassIsRetried = false;
 
-    auto* sprite = cocos2d::CCSprite::create("dialogIcon_017.png");
-    sprite->setPosition(ccp(100, 100));
-    sprite->setScale(0.5f);
-    self->addChild(sprite);
+    bool init(gd::LevelBrowserLayer* self, GJSearchObject* searchParameters) {
+        if (!matdash::orig<&init>(self, searchParameters)) return false;
 
-    auto* label = cocos2d::CCLabelBMFont::create("Hello world!", "bigFont.fnt");
-    label->setPosition(ccp(100, 150));
-    label->setAnchorPoint(ccp(0, 0.5));
-    self->addChild(label);
+        buttonWasCreated = false;
 
-    return true;
+        return true;
+    }
+
 }
 
 void mod_main(HMODULE) {
     // this creates a console window whenever the mod is injected
     // which is very useful for debugging, but make sure to remove
     // on release builds! :D
-    matdash::create_console();
+    if(DEBUG) matdash::create_console();
 
-    matdash::add_hook<&MenuLayer_onNewgrounds>(gd::base + 0x191e90);
-    matdash::add_hook<&GJDropDownLayer_init>(gd::base + 0x113530);
-    matdash::add_hook<&PlayLayer_update>(gd::base + 0x2029c0);
-    matdash::add_hook<&MenuLayer_init>(gd::base + 0x1907B0);
+    t = new cocos2d::CCObject();
+
+    matdash::add_hook<&MenuLayerX::MenuLayer_init>(gd::base + 0x1907B0);
+    matdash::add_hook<&LevelInfoX::init>(gd::base + 0x175DF0);
+    matdash::add_hook<&ProfilePageX::init>(gd::base + 0x20EF00);
+
+    if (DEBUG) std::cout << "[LOG] Early init success!" << std::endl;
 }
